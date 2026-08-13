e84c9560847a01ace025a4aa0c49ee391f7d2153
saleemmughal17 <saleem177mughal@gmai.com>
2026-08-13 17:25:28 +0500

    fixed mansory

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -1634,7 +1634,7 @@ export default async function ServiceDetailPage({
                   <div className="w-full lg:w-1/2 relative min-h-[350px] lg:min-h-full overflow-hidden">
                     <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-white to-transparent lg:block hidden z-10 w-24" />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-white to-transparent lg:hidden block z-10 h-24" />
-                      <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Brick & Block Work")} />
+                      <Image src={t("/images/services/masonry_brickwork.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Brick & Block Work")} />
                   </div>
                 </AnimatedCard>
 
@@ -1643,7 +1643,7 @@ export default async function ServiceDetailPage({
                   <div className="w-full lg:w-1/2 order-2 lg:order-1 relative min-h-[350px] lg:min-h-full overflow-hidden">
                     <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-white to-transparent lg:block hidden z-10 w-24" />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-white to-transparent lg:hidden block z-10 h-24" />
-                      <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Plastering Services")} />
+                      <Image src={t("/images/services/masonry_plastering.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Plastering Services")} />
                   </div>
                   <div className="w-full lg:w-1/2 order-1 lg:order-2 p-10 lg:p-16 space-y-6 flex flex-col justify-center relative z-10 bg-white">
                     <div className="inline-flex items-center gap-4 mb-2">
@@ -1680,7 +1680,7 @@ export default async function ServiceDetailPage({
                   <div className="w-full lg:w-1/2 relative min-h-[350px] lg:min-h-full overflow-hidden">
                     <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-white to-transparent lg:block hidden z-10 w-24" />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-white to-transparent lg:hidden block z-10 h-24" />
-                      <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Tile Install & Repair")} />
+                      <Image src={t("/images/services/masonry_tiling.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Tile Install & Repair")} />
                   </div>
                 </AnimatedCard>
 
@@ -1689,7 +1689,7 @@ export default async function ServiceDetailPage({
                   <div className="w-full lg:w-1/2 order-2 lg:order-1 relative min-h-[350px] lg:min-h-full overflow-hidden">
                     <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-white to-transparent lg:block hidden z-10 w-24" />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-white to-transparent lg:hidden block z-10 h-24" />
-                      <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Concrete Works")} />
+                      <Image src={t("/images/services/masonry_concrete.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Concrete Works")} />
                   </div>
                   <div className="w-full lg:w-1/2 order-1 lg:order-2 p-10 lg:p-16 space-y-6 flex flex-col justify-center relative z-10 bg-white">
                     <div className="inline-flex items-center gap-4 mb-2">
@@ -1726,7 +1726,7 @@ export default async function ServiceDetailPage({
                   <div className="w-full lg:w-1/2 relative min-h-[350px] lg:min-h-full overflow-hidden">
                     <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-white to-transparent lg:block hidden z-10 w-24" />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-white to-transparent lg:hidden block z-10 h-24" />
-                      <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Renovation Services")} />
+                      <Image src={t("/images/services/masonry_renovations.png")} fill sizes="(max-width: 1024px) 100vw, 50vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Renovation Services")} />
                   </div>
                 </AnimatedCard>
 
@@ -1810,7 +1810,7 @@ export default async function ServiceDetailPage({
                 </div>
                 <div className="lg:col-span-5 xl:col-span-4">
                   <div className="relative aspect-[4/5] rounded-3xl overflow-hidden group">
-                    <Image src={t("/images/services/masonry.png")} fill sizes="(max-width: 1024px) 100vw, 33vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Service Workflow")} />
+                    <Image src={t("/images/services/masonry_workflow.png")} fill sizes="(max-width: 1024px) 100vw, 33vw" className="object-cover transition-transform duration-700 group-hover:scale-105" alt={t("Service Workflow")} />
                   </div>
                 </div>
               </div>
